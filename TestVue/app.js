8fd60ae0306d118010513783617565c5aad4f29f
Sivuyile Magutywa <s.magutywa@kernet.co.za>
2020-03-08 20:29:20 +0200

    Initial project version

---
+++ TestVue/app.js
@@ -0,0 +1,12 @@
+new Vue({
+    el: '#app-vue',
+    data: {
+      message: 'Hello Vue!',
+
+      showHome: false,
+    showCheck: false,
+    showCoin: false,
+    showUser: false,
+    effect: 'lni-tada-effect'
+    }
+  });
